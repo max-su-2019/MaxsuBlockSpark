0f8df1b2e6fde3e788d719fceafdb703c8605300
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2021-11-25 23:10:00 +0800

    Update to v0.221
    Allow dual wielding or left hand blocking trigger sparks effect.

--- src/Events.cpp
+++ src/Events.cpp
@@ -6,6 +6,7 @@ namespace MaxsuBlockSpark
 	EventResult OnHitEventHandler::ProcessEvent(const RE::TESHitEvent* a_event, RE::BSTEventSource<RE::TESHitEvent>* a_eventSource)
 	{
 		using HitFlag = RE::TESHitEvent::Flag;
+		using AttackState = RE::ATTACK_STATE_ENUM;
 
 		if (!a_event || !a_eventSource) {
 			logger::error("Event Source Not Found!");
@@ -17,7 +18,7 @@ namespace MaxsuBlockSpark
 		if (a_event->flags.any(HitFlag::kHitBlocked) && a_event->target) {
 			auto defender = a_event->target->As<RE::Actor>();
 			auto attackWeapon = RE::TESForm::LookupByID<RE::TESObjectWEAP>(a_event->source);
-			if (!defender || !attackWeapon || !defender->currentProcess || !attackWeapon->IsMelee() || attackWeapon->IsHandToHandMelee() || !defender->Get3D())
+			if (!defender || !attackWeapon || !defender->currentProcess || !defender->currentProcess->high || !attackWeapon->IsMelee() || attackWeapon->IsHandToHandMelee() || !defender->Get3D())
 				return EventResult::kContinue;
 
 			auto attacker = a_event->cause ? a_event->cause->As<RE::Actor>(): nullptr;
@@ -26,23 +27,23 @@ namespace MaxsuBlockSpark
 				return EventResult::kContinue;
 			}
 
-			auto attackData = attacker->currentProcess->high->attackData;
-			if (!attackData) {
-				logger::debug("Attack Data Not Found!");
+			auto attackerData = attacker->currentProcess->high->attackData;
+			if (!attackerData) {
+				logger::debug("Attacker Attack Data Not Found!");
 				return EventResult::kContinue;
 			}
 			 
-			auto attackerNode = attackData->IsLeftAttack() ? attacker->GetNodeByName("SHIELD") : attacker->GetNodeByName("WEAPON");
+			auto attackerNode = attackerData->IsLeftAttack() ? attacker->GetNodeByName("SHIELD") : attacker->GetNodeByName("WEAPON");
 			if (!attackerNode) {
 				logger::debug("Attacker Node Not Found!");
 				return EventResult::kContinue;
 			}
 
-			auto GetBipeObjIndex = [](RE::TESForm* parryEquipment) -> RE::BIPED_OBJECT {
+			auto GetBipeObjIndex = [](RE::TESForm* parryEquipment, bool rightHand) -> RE::BIPED_OBJECT {
 				if (!parryEquipment)
 					return RE::BIPED_OBJECT::kNone;
 
-				if (parryEquipment->As<RE::TESObjectWEAP>()) {
+				if (parryEquipment->As<RE::TESObjectWEAP>() && rightHand) {
 					switch (parryEquipment->As<RE::TESObjectWEAP>()->GetWeaponType()) {
 					case RE::WEAPON_TYPE::kOneHandSword:
 						return RE::BIPED_OBJECT::kOneHandSword;
@@ -57,13 +58,21 @@ namespace MaxsuBlockSpark
 					case RE::WEAPON_TYPE::kTwoHandSword:
 						return RE::BIPED_OBJECT::kTwoHandMelee;
 					}
-				} else if (parryEquipment->As<RE::TESObjectARMO>() && parryEquipment->As<RE::TESObjectARMO>()->IsShield())
+				} else
 					return RE::BIPED_OBJECT::kShield;
 
 				return RE::BIPED_OBJECT::kNone;
 			};
 
-			auto BipeObjIndex = defender->currentProcess->GetEquippedLeftHand() ? GetBipeObjIndex(defender->currentProcess->GetEquippedLeftHand()) : GetBipeObjIndex(defender->currentProcess->GetEquippedRightHand());
+			RE::BIPED_OBJECT BipeObjIndex;
+			auto defenderLeftEquipped = defender->currentProcess->GetEquippedLeftHand();
+			auto defenderData = defender->currentProcess->high->attackData;
+			
+			if (defenderData && defenderLeftEquipped) 
+				BipeObjIndex = defenderData->IsLeftAttack() && (defenderLeftEquipped->IsWeapon() || defenderLeftEquipped->IsArmor()) ? GetBipeObjIndex(defenderLeftEquipped, false) : GetBipeObjIndex(defender->currentProcess->GetEquippedRightHand(), true);
+			else 
+				BipeObjIndex = defenderLeftEquipped && (defenderLeftEquipped->IsWeapon() || defenderLeftEquipped->IsArmor()) ? GetBipeObjIndex(defenderLeftEquipped, false) : GetBipeObjIndex(defender->currentProcess->GetEquippedRightHand(), true);
+
 			if (BipeObjIndex == RE::BIPED_OBJECT::kNone) {
 				logger::debug("BipeObj Not Found!");
 				return EventResult::kContinue;
@@ -82,7 +91,7 @@ namespace MaxsuBlockSpark
 			RE::NiPoint3 sparkPos;
 			RE::NiPoint3 hitPos = attackerNode->worldBound.center + attackerNode->world.rotate * RE::NiPoint3(0.f,0.5f * attackerNode->worldBound.radius,0.f);
 
-			if (BipeObjIndex == RE::BIPED_OBJECT::kShield && defender->GetAttackState() == RE::ATTACK_STATE_ENUM::kNone && SparkLocalizer::GetShieldSparkPos(hitPos, defenderNode.get(), sparkPos))
+			if (BipeObjIndex == RE::BIPED_OBJECT::kShield && defenderLeftEquipped && defenderLeftEquipped->IsArmor() && SparkLocalizer::GetShieldSparkPos(hitPos, defenderNode.get(), sparkPos))
 				logger::debug("Get Shield Spark Position!");
 			else {
 				sparkPos = defenderNode->worldBound.center;
